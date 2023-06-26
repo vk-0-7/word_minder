d48eb45436a72d89912b355c8a2d92541b333ce7
Vivek Kumar <vivekr4400@gmail.com>
2023-06-26 11:41:26 +0530

    changed backend url

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,1 +1,2 @@
-export const BASE_URL ='https://reviserbackend.onrender.com/';
+export const BASE_URl ='https://reviserbackend.onrender.com/';
+export const BASE_URL='https://reviser-backend-production.up.railway.app/'
